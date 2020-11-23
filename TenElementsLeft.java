1dd08619c0607feb275ba42fb4bb853f17c409cd
yana-kroasana <72204689+yana-kroasana@users.noreply.github.com>
2020-11-23 18:06:47 +0200

    Add files via upload

---
+++ TenElementsLeft.java
@@ -0,0 +1,26 @@
+package info;
+
+import java.util.Scanner;
+
+public class TenElementsLeft {
+
+	public static void main(String[] args) {
+		int[] array = new int[10];
+		Scanner sc = new Scanner(System.in);
+		for(int i = 0; i < 10; i++) {
+			array[i] = sc.nextInt();
+		}
+		
+		int temp = array[0];
+		
+		for(int i = 0; i < array.length - 1; i++) {
+			array[i] = array[i + 1];
+		}
+		
+		array[array.length -1] = temp;
+		
+		for( int i = 0; i < 10; i++ ) {
+			System.out.print(array[i] + " ");
+		}
+	}
+}
